b01b23dab69ee8026bf4055dc1bc500d06aedfc8
Akkun716 <aanglon@dons.usfca.edu>
2021-10-22 05:53:36 -0700

    Moved inverted index out to builder file

--- src/main/java/InvertedIndex.java
+++ src/main/java/InvertedIndex.java
@@ -2,23 +2,12 @@ import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
-/*
- * TODO
+/**
+ * TODO:
  * Move the string/file/directory parsing logic into an builder or factory
  * class (InvertedIndexBuidler)
- *
- * Built out the data structure methods in this class a bit more
- * toString
- * add(String word, String location, Integer position)
- * 3x contains or has methods
- * 3x size or num methods
- * 3x safe get methods (can make it safe later)
  */
 
 /**
@@ -33,11 +22,9 @@ public class InvertedIndex {
 	 * will hold file locations as keys and arrayList of Integers as values. These
 	 * Integers represent the position of the stemmed word occurrences
 	 */
-	// TODO ArrayList --> Set
 	// TODO Create a constructor and initialize it there
-	// TODO final
-	private Map<String, TreeMap<String, ArrayList<Integer>>> invertedIndex =
-			new TreeMap<>();
+	
+	public final InvertedIndexBuilder invertedIndex = new InvertedIndexBuilder();
 
 	/**
 	 * Takes in a Path object and uses TextStemmer to parse through the text file(s) indicated
@@ -59,17 +46,9 @@ public class InvertedIndex {
 					}
 				}
 			}
-			catch(IOException e) { // TODO Remove catch blocks
-				System.out.println("[Directory could not be found]");
-			}
 		}
 		else {
-			try {
-				passToIndex(mainPath, TextStemmer.listStems(mainPath));
-			}
-			catch(IOException e) { // TODO Remove catch blocks
-				System.out.println("[File could not be found]");
-			}
+			passToIndex(mainPath, TextStemmer.listStems(mainPath));
 		}
 	}
 
@@ -84,24 +63,8 @@ public class InvertedIndex {
 	private void passToIndex(Path path, List<String> input) {
 		int i = 1;
 		for(String stem: input) {
-			if(!invertedIndex.containsKey(stem)) {
-				invertedIndex.put(stem, new TreeMap<>());
-			}
-			if(!invertedIndex.get(stem).containsKey(path.toString())) {
-				invertedIndex.get(stem).put(path.toString(), new ArrayList<Integer>());
-			}
-			invertedIndex.get(stem).get(path.toString()).add(i++);
+			invertedIndex.add(stem, path.toString(), i++);
 		}
-	}
-
-	/**
-	 * Returns an unmodifiable invertedIndex for easy printing.
-	 *
-	 * @return an unmodifiable map that can be easily viewed for quick printing
-	 */
-
-	public Map<String, Map<String, ArrayList<Integer>>> getMap() {
-		return Collections.unmodifiableMap(invertedIndex);
 	}
 
 	/**
@@ -112,6 +75,6 @@ public class InvertedIndex {
 	 * @throws IOException file is invalid or can not be found
 	 */
 	public void writeFile(Path output) throws IOException {
-			JsonWriter.asNestedObject(invertedIndex, output);
+			JsonWriter.asNestedObject(invertedIndex.getIndex(), output);
 	}
 }
