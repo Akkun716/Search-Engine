2b57fdcdc1ab457fa75c915bab29c6875b570c86
Sophie Engle <sjengle@cs.usfca.edu>
2021-10-25 15:01:19 -0700

    Resubmit (Sync)

--- src/main/java/InvertedIndexBuilder.java
+++ src/main/java/InvertedIndexBuilder.java
@@ -4,15 +4,20 @@ import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
+/*
+ * TODO Refactor this one to InvertedIndex
+ */
+
 /**
  * This class represents the data structure of the inverted index.
  */
 public class InvertedIndexBuilder {
+	// TODO Init invertedIndex in the constructor
 	/**
 	 * This map hold the data of the inverted index. The over-arching map holds
 	 * word stem keys which are paired to respective maps that has file location
 	 * keys which are then paired to an array of positions in the file the word
-	 * stem appeared  
+	 * stem appeared
 	 */
 	private final Map<String, TreeMap<String, TreeSet<Integer>>> invertedIndex =
 			new TreeMap<>();
@@ -20,7 +25,7 @@ public class InvertedIndexBuilder {
 	/**
 	 * Adds  word keys, file location keys, and positions in the index
 	 * (if they does not already exist in the index)
-	 * 
+	 *
 	 * @param word stemmed word
 	 * @param location file location where the word stem appeared
 	 * @param position position of the word stem in respective file location
@@ -31,16 +36,24 @@ public class InvertedIndexBuilder {
 		invertedIndex.get(word).putIfAbsent(location, new TreeSet<Integer>());
 		return invertedIndex.get(word).get(location).add(position);
 	}
-	
+
+	/*
+	 * TODO
+	 *
+	 * public boolean add or addAll(List<String> words, String location) <--- call add in a loop
+	 */
+
+	// TODO Still breaking encapsulation
 	/**
 	 * Returns an unmodifiable invertedIndex for easy printing.
 	 *
 	 * @return an unmodifiable index map
 	 */
 	public Map<String, TreeMap<String, TreeSet<Integer>>> getIndex() {
+		// TODO getWords() --> returns an unmodifiable set of the invertedIndex.keySet()
 		return Collections.unmodifiableMap(invertedIndex);
 	}
-	
+
 	/**
 	 * Returns an unmodifiable map of file locations of a word stem
 	 *
@@ -48,9 +61,10 @@ public class InvertedIndexBuilder {
 	 * @return an unmodifiable Set of positions
 	 */
 	public Map<String, TreeSet<Integer>> getLocations(String stem) {
+		// TODO return an unmodifiable view of invertedIndex.get(stem).keySet()
 		return Collections.unmodifiableMap(invertedIndex.getOrDefault(stem, new TreeMap<>()));
 	}
-	
+
 	/**
 	 * Returns an unmodifiable Set of word stem positions at file location
 	 *
@@ -61,22 +75,23 @@ public class InvertedIndexBuilder {
 	public Set<Integer> getPositions(String stem, String location) {
 		return hasLocation(stem, location)
 				? Collections.unmodifiableSet(invertedIndex.get(stem).get(location))
+						// TODO Collections.emptySet();
 				: Collections.unmodifiableSet(new TreeSet<>());
 	}
-	
+
 	/**
 	 * Checks if word stem exists as a key in index
-	 * 
+	 *
 	 * @param stem word stem to be found in index
 	 * @return true if word stem exists as key in map
 	 */
 	public boolean hasStem(String stem) {
 		return invertedIndex.containsKey(stem);
 	}
-	
+
 	/**
 	 * Checks if the word stems exists and then if location can be found
-	 *  
+	 *
 	 * @param stem word stem to be found in index
 	 * @param location file location to be found under word stem key
 	 * @return true if location exists under word stem key
@@ -84,11 +99,11 @@ public class InvertedIndexBuilder {
 	public boolean hasLocation(String stem, String location) {
 		return hasStem(stem) && invertedIndex.get(stem).containsKey(location);
 	}
-	
+
 	/**
 	 * Checks if the word and location keys exists in index and then if position
 	 * can be found
-	 * 
+	 *
 	 * @param stem word stem to be found in index
 	 * @param location file location to be found under word stem key
 	 * @param position position of the word stem at designated file location
@@ -98,19 +113,19 @@ public class InvertedIndexBuilder {
 		return hasLocation(stem, location)
 				&& invertedIndex.get(stem).get(location).contains(position);
 	}
-	
+
 	/**
 	 * Returns number of word stem keys in index
-	 * 
+	 *
 	 * @return size of invertedIndex map
 	 */
 	public int stemCount() {
 		return invertedIndex.size();
 	}
-	
+
 	/**
 	 * Returns number of file locations under word stem key in index
-	 * 
+	 *
 	 * @param stem word stem key to be accessed
 	 * @return size of map assigned to word stem if exists; else 0
 	 */
@@ -119,11 +134,11 @@ public class InvertedIndexBuilder {
 				? invertedIndex.get(stem).size()
 				: 0;
 	}
-	
+
 	/**
 	 * Returns number of positions under respective file location and word stem
 	 * keys in index
-	 * 
+	 *
 	 * @param stem word stem key to be accessed
 	 * @param location file location to be found under word stem key
 	 * @return size of set assigned to location of word stem if exists; else 0
@@ -133,9 +148,15 @@ public class InvertedIndexBuilder {
 				? invertedIndex.get(stem).get(location).size()
 				: 0;
 	}
-	
+
 	@Override
 	public String toString() {
 		return this.invertedIndex.toString();
 	}
+
+	/* TODO
+	public void toJson(Path output) throws IOException {
+		JsonWriter.asNestedObject(invertedIndex, output);
+	}
+	*/
 }
