387e0637b92c3ae37ca4423a5ace5728b8f38ee3
Akkun716 <aanglon@dons.usfca.edu>
2021-10-30 17:41:06 -0700

    Added wordCount Json output

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -41,10 +41,20 @@ public class Driver {
 		}
 		InvertedIndex index = builder.build();
 		
+		if(map.hasFlag("-counts")) {
+			Path output = map.getPath("-counts", Path.of("counts.json"));
+			try {
+				index.countToJson(output);
+			}
+			catch(Exception e) {
+				System.out.println("Unable to write out to file: " + output.toString());
+			}
+		}
+		
 		if(map.hasFlag("-index")) {
 			Path output = map.getPath("-index", Path.of("index.json"));
 			try {
-				index.toJson(output);
+				index.indexToJson(output);
 			}
 			catch(Exception e) {
 				System.out.println("Unable to write out to file: " + output.toString());
