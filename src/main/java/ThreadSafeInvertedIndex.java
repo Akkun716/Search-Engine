9030c9c7b1af19c1efb6552335297ae9e1cc04c3
Akkun716 <aanglon@dons.usfca.edu>
2021-11-28 17:56:24 -0800

    Updated with project_3 barnch functionality

---
+++ src/main/java/ThreadSafeInvertedIndex.java
@@ -0,0 +1,121 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Set;
+
+public class ThreadSafeInvertedIndex extends InvertedIndex{
+	WorkQueue queue;
+	IndexReadWriteLock lock;
+	
+	public ThreadSafeInvertedIndex() {
+		this(new WorkQueue());
+	}
+	
+	public ThreadSafeInvertedIndex(WorkQueue queue) {
+		this.queue = queue;
+		lock = new IndexReadWriteLock();
+	}
+	
+	@Override
+	public boolean add(String word, String location, Integer position) {
+		synchronized(lock.writeLock()) {
+			return super.add(word, location, position);
+		}
+	}
+
+	@Override
+	public List<QueryResult> exactSearch(Set<String> elem) {
+		synchronized(lock.readLock()) {
+			return super.exactSearch(elem);
+		}
+	}
+
+	@Override
+	public List<QueryResult> partialSearch(Set<String> elem) {
+		synchronized(lock.readLock()) {
+			return super.partialSearch(elem);
+		}
+	}
+
+	@Override
+	public Set<String> getWords() {
+		synchronized(lock.readLock()) {
+			return super.getWords();
+		}
+	}
+
+	@Override
+	public Set<String> getLocations(String stem) {
+		synchronized(lock.readLock()) {
+			return super.getLocations(stem);
+		}
+	}
+
+	@Override
+	public Set<Object> getPositions(String stem, String location) {
+		synchronized(lock.readLock()) {
+			return super.getPositions(stem, location);
+		}
+	}
+
+	@Override
+	public boolean hasStem(String stem) {
+		synchronized(lock.readLock()) {
+			return super.hasStem(stem);
+		}
+	}
+
+	@Override
+	public boolean hasLocation(String stem, String location) {
+		synchronized(lock.readLock()) {
+			return super.hasLocation(stem, location);
+		}
+	}
+
+	@Override
+	public boolean hasPosition(String stem, String location, Integer position) {
+		synchronized(lock.readLock()) {
+			return super.hasPosition(stem, location, position);
+		}
+	}
+
+	@Override
+	public int stemCount() {
+		synchronized(lock.readLock()) {
+			return super.stemCount();
+		}
+	}
+
+	@Override
+	public int locationCount(String stem) {
+		synchronized(lock.readLock()) {
+			return super.locationCount(stem);
+		}
+	}
+
+	@Override
+	public int positionCount(String stem, String location) {
+		synchronized(lock.readLock()) {
+			return super.positionCount(stem, location);
+		}
+	}
+
+	@Override
+	public String toString() {
+		synchronized(lock.readLock()) {
+			return super.toString();
+		}
+	}
+
+	@Override
+	public void indexToJson(Path output) throws IOException {
+		// TODO Auto-generated method stub
+		super.indexToJson(output);
+	}
+
+	@Override
+	public void countToJson(Path output) throws IOException {
+		// TODO Auto-generated method stub
+		super.countToJson(output);
+	}
+}
